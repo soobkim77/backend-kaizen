b05e10660714ed40882d3aca82f351f269ea8454
Soo-Beom <soobeomkim77@gmail.com>
2021-05-18 17:20:06 -0700

    added user validations

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+    skip_before_action :authorized
+
+    def create 
+        @user = User.create(user_params) 
+        if @user.valid?
+            render json: {login: true}, status: :created
+        else
+            render json: {message: "You fucked up."}, status: :not_acceptable
+        end
+    end
+
+    def index
+        if current_user
+        render json: UserSerializer.new(@@user), status: :accepted
+        end
+    end
+    
+   
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Formulating Kai's"
+
+5.times do 
+    User.create(username: Faker::Name.first_name, password: "1234")
+end
