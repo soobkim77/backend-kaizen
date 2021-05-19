234aab195f52b8cf2347b836e97d8d85ea7aedf2
Soo-Beom <soobeomkim77@gmail.com>
2021-05-19 14:32:11 -0700

    created models and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
-
+    has_many :boards
+    has_many :tasks, through: :boards
 end
